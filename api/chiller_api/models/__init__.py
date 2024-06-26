3c6169e9c83384cb78965dfd1e584b4de2ac0ede
Jonathan Manton <jonathan@manton.com>
2024-06-26 13:26:47 -0500

    after regenerating api for 1.0.2

--- api/chiller_api/models/__init__.py
+++ api/chiller_api/models/__init__.py
@@ -5,4 +5,5 @@ from __future__ import absolute_import
 # import models into model package
 from chiller_api.models.jwt import JWT
 from chiller_api.models.movie import Movie
+from chiller_api.models.movie_list import MovieList
 from chiller_api.models.user import User
